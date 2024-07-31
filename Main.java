736852c2bc3ba97c49d4873860f4e0bb9489e176
Valki11 <102394314+Valki11@users.noreply.github.com>
2024-07-30 21:29:47 -0600

    Cambios

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+package vista;
+
+/**
+ *
+ * @author keila
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+        forn frn = new forn();
+        frn.show();
+        
+    }
+    }
